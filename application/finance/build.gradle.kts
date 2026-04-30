f90249b899bb09ff828a950fd63da6779fb7a0ed
Ketan Verma <ketan.lkct@gmail.com>
2026-05-01 00:56:00 +0530

    Update

--- application/finance/build.gradle.kts
+++ application/finance/build.gradle.kts
@@ -4,20 +4,13 @@ plugins {
 
 dependencies {
     implementation(enforcedPlatform("io.quarkus.platform:quarkus-bom:3.9.4"))
-
-    // Core Quarkus
     implementation("io.quarkus:quarkus-arc")
     implementation("io.quarkus:quarkus-rest")
     implementation("io.quarkus:quarkus-rest-jackson")
-
-    // Persistence
     implementation("io.quarkus:quarkus-hibernate-orm-panache")
     implementation("io.quarkus:quarkus-jdbc-postgresql")
-
-    // Shared module
+    implementation("io.quarkus:quarkus-flyway")
+    implementation("io.quarkus:quarkus-smallrye-openapi")
     implementation(project(":shared"))
-
-    // Test
     testImplementation("io.quarkus:quarkus-junit5")
-    testImplementation("io.rest-assured:rest-assured")
 }
